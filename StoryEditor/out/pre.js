a7e30eb9b29213b8ec5e78cf7695fe29b5c177bf
Claeb <87375599+turbonobsin@users.noreply.github.com>
2026-02-04 22:59:19 -0500

    update to new hosting 2026 - crazy haha

--- StoryEditor/out/pre.js
+++ StoryEditor/out/pre.js
@@ -1,6 +1,8 @@
-// let serverURL = "http://localhost:3000";
-let serverURL = "https://next-overly-urchin.ngrok-free.app";
+// let serverURL = "http://localhost:3001";
+// let serverURL = "https://claeb-rog.tail60cfd4.ts.net/";
+// let serverURL = "https://next-overly-urchin.ngrok-free.app";
 // let serverURL = "https://claebcode.top:3001";
+let serverURL = localStorage.getItem("customURL") ?? "https://1adb-185-73-8-100.ngrok-free.app/";
 const AID = "__SE-"; // app id
 let page = 0;
 let isOffline = true;
