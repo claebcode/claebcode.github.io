cbc4b7363c2f92db3a85f091c491fd3bea651aa2
Claeb <claebhero@gmail.com>
2026-02-16 14:46:28 -0500

    updated server url

--- StoryEditor/out/pre.js
+++ StoryEditor/out/pre.js
@@ -2,7 +2,8 @@
 // let serverURL = "https://claeb-rog.tail60cfd4.ts.net/";
 // let serverURL = "https://next-overly-urchin.ngrok-free.app";
 // let serverURL = "https://claebcode.top:3001";
-let serverURL = localStorage.getItem("customURL") ?? "https://1adb-185-73-8-100.ngrok-free.app";
+// let serverURL = localStorage.getItem("customURL") ?? "https://1adb-185-73-8-100.ngrok-free.app";
+let serverURL = localStorage.getItem("customURL") ?? "https://8d45-185-73-8-100.ngrok-free.app";
 const AID = "__SE-"; // app id
 let page = 0;
 let isOffline = true;
